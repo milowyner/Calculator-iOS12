a355e20189532f38ee35f5e21b58ad6766538c5e
Milo Wyner <milowyner@gmail.com>
2018-11-29 17:51:45 -0800

    Enhance UI for iPhone X-type phones

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,8 +11,26 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var safeAreaBlur: UIStackView!
     
-    
+    override func viewDidLoad() {
+        print("hello")
+        // 1
+        safeAreaBlur.backgroundColor = .clear
+        // 2
+        let blurEffect = UIBlurEffect(style: .dark)
+        // 3
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        // 4
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        safeAreaBlur.insertSubview(blurView, at: 0)
+
+        NSLayoutConstraint.activate([
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            ])
+
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
@@ -24,7 +42,9 @@ class ViewController: UIViewController {
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        
+        displayLabel.text = sender.titleLabel?.text
+        
     }
 
 }
